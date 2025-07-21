0c8c46fc44e7cd44bd8f84468723acb6399909b3
nathanalvares19 <nathanalvares2706@gmail.com>
2025-07-21 23:46:18 +0530

    results page v1

---
+++ elan-quest/app/components/results/Process.tsx
@@ -0,0 +1,104 @@
+export default function Process() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      // width="513"
+      // height="63"
+      viewBox="0 0 513 63"
+      fill="none"
+      className="w-[300px] h-auto"
+    >
+      <path d="M75 50H87.5V62.5H75V50Z" fill="#E8E8C6" />
+      <path d="M0 50H12.5V62.5H0V50Z" fill="#E8E8C6" />
+      <path d="M75 37.5H87.5V50H75V37.5Z" fill="#E8E8C6" />
+      <path d="M0 37.5H12.5V50H0V37.5Z" fill="#E8E8C6" />
+      <path d="M150 37.5H162.5V50H150V37.5Z" fill="#E8E8C6" />
+      <path d="M200 37.5H212.5V50H200V37.5Z" fill="#E8E8C6" />
+      <path d="M75 25H87.5V37.5H75V25Z" fill="#E8E8C6" />
+      <path d="M0 25H12.5V37.5H0V25Z" fill="#E8E8C6" />
+      <path d="M150 25H162.5V37.5H150V25Z" fill="#E8E8C6" />
+      <path d="M200 25H212.5V37.5H200V25Z" fill="#E8E8C6" />
+      <path d="M87.5 25H100V37.5H87.5V25Z" fill="#E8E8C6" />
+      <path d="M12.5 25H25V37.5H12.5V25Z" fill="#E8E8C6" />
+      <path d="M387.5 25H400V37.5H387.5V25Z" fill="#E8E8C6" />
+      <path d="M462.5 25H475V37.5H462.5V25Z" fill="#E8E8C6" />
+      <path d="M387.5 50H400V62.5H387.5V50Z" fill="#E8E8C6" />
+      <path d="M462.5 50H475V62.5H462.5V50Z" fill="#E8E8C6" />
+      <path d="M375 50H387.5V62.5H375V50Z" fill="#E8E8C6" />
+      <path d="M450 50H462.5V62.5H450V50Z" fill="#E8E8C6" />
+      <path d="M87.5 0H100V12.5H87.5V0Z" fill="#E8E8C6" />
+      <path d="M12.5 0H25V12.5H12.5V0Z" fill="#E8E8C6" />
+      <path d="M387.5 0H400V12.5H387.5V0Z" fill="#E8E8C6" />
+      <path d="M462.5 0H475V12.5H462.5V0Z" fill="#E8E8C6" />
+      <path d="M162.5 0H175V12.5H162.5V0Z" fill="#E8E8C6" />
+      <path d="M162.5 50H175V62.5H162.5V50Z" fill="#E8E8C6" />
+      <path d="M100 25H112.5V37.5H100V25Z" fill="#E8E8C6" />
+      <path d="M25 25H37.5V37.5H25V25Z" fill="#E8E8C6" />
+      <path d="M400 25H412.5V37.5H400V25Z" fill="#E8E8C6" />
+      <path d="M475 25H487.5V37.5H475V25Z" fill="#E8E8C6" />
+      <path d="M400 50H412.5V62.5H400V50Z" fill="#E8E8C6" />
+      <path d="M475 50H487.5V62.5H475V50Z" fill="#E8E8C6" />
+      <path d="M112.5 37.5H125V50H112.5V37.5Z" fill="#E8E8C6" />
+      <path d="M425 37.5H437.5V50H425V37.5Z" fill="#E8E8C6" />
+      <path d="M500 37.5H512.5V50H500V37.5Z" fill="#E8E8C6" />
+      <path d="M100 0H112.5V12.5H100V0Z" fill="#E8E8C6" />
+      <path d="M25 0H37.5V12.5H25V0Z" fill="#E8E8C6" />
+      <path d="M400 0H412.5V12.5H400V0Z" fill="#E8E8C6" />
+      <path d="M475 0H487.5V12.5H475V0Z" fill="#E8E8C6" />
+      <path d="M175 0H187.5V12.5H175V0Z" fill="#E8E8C6" />
+      <path d="M175 50H187.5V62.5H175V50Z" fill="#E8E8C6" />
+      <path d="M112.5 25H125V37.5H112.5V25Z" fill="#E8E8C6" />
+      <path d="M37.5 25H50V37.5H37.5V25Z" fill="#E8E8C6" />
+      <path d="M412.5 25H425V37.5H412.5V25Z" fill="#E8E8C6" />
+      <path d="M487.5 25H500V37.5H487.5V25Z" fill="#E8E8C6" />
+      <path d="M412.5 50H425V62.5H412.5V50Z" fill="#E8E8C6" />
+      <path d="M487.5 50H500V62.5H487.5V50Z" fill="#E8E8C6" />
+      <path d="M125 50H137.5V62.5H125V50Z" fill="#E8E8C6" />
+      <path d="M112.5 0H125V12.5H112.5V0Z" fill="#E8E8C6" />
+      <path d="M37.5 0H50V12.5H37.5V0Z" fill="#E8E8C6" />
+      <path d="M412.5 0H425V12.5H412.5V0Z" fill="#E8E8C6" />
+      <path d="M487.5 0H500V12.5H487.5V0Z" fill="#E8E8C6" />
+      <path d="M425 0H437.5V12.5H425V0Z" fill="#E8E8C6" />
+      <path d="M500 0H512.5V12.5H500V0Z" fill="#E8E8C6" />
+      <path d="M187.5 0H200V12.5H187.5V0Z" fill="#E8E8C6" />
+      <path d="M187.5 50H200V62.5H187.5V50Z" fill="#E8E8C6" />
+      <path d="M125 12.5H137.5V25H125V12.5Z" fill="#E8E8C6" />
+      <path d="M50 12.5H62.5V25H50V12.5Z" fill="#E8E8C6" />
+      <path d="M75 12.5H87.5V25H75V12.5Z" fill="#E8E8C6" />
+      <path d="M0 12.5H12.5V25H0V12.5Z" fill="#E8E8C6" />
+      <path d="M375 12.5H387.5V25H375V12.5Z" fill="#E8E8C6" />
+      <path d="M450 12.5H462.5V25H450V12.5Z" fill="#E8E8C6" />
+      <path d="M150 12.5H162.5V25H150V12.5Z" fill="#E8E8C6" />
+      <path d="M200 12.5H212.5V25H200V12.5Z" fill="#E8E8C6" />
+      <path d="M75 0H87.5V12.5H75V0Z" fill="#E8E8C6" />
+      <path d="M0 0H12.5V12.5H0V0Z" fill="#E8E8C6" />
+      <path d="M300 50H312.5V62.5H300V50Z" fill="#E8E8C6" />
+      <path d="M225 37.5H237.5V50H225V37.5Z" fill="#E8E8C6" />
+      <path d="M300 37.5H312.5V50H300V37.5Z" fill="#E8E8C6" />
+      <path d="M225 25H237.5V37.5H225V25Z" fill="#E8E8C6" />
+      <path d="M300 25H312.5V37.5H300V25Z" fill="#E8E8C6" />
+      <path d="M312.5 25H325V37.5H312.5V25Z" fill="#E8E8C6" />
+      <path d="M237.5 0H250V12.5H237.5V0Z" fill="#E8E8C6" />
+      <path d="M312.5 0H325V12.5H312.5V0Z" fill="#E8E8C6" />
+      <path d="M237.5 50H250V62.5H237.5V50Z" fill="#E8E8C6" />
+      <path d="M312.5 50H325V62.5H312.5V50Z" fill="#E8E8C6" />
+      <path d="M325 25H337.5V37.5H325V25Z" fill="#E8E8C6" />
+      <path d="M275 0H287.5V12.5H275V0Z" fill="#E8E8C6" />
+      <path d="M350 0H362.5V12.5H350V0Z" fill="#E8E8C6" />
+      <path d="M275 50H287.5V62.5H275V50Z" fill="#E8E8C6" />
+      <path d="M350 50H362.5V62.5H350V50Z" fill="#E8E8C6" />
+      <path d="M250 0H262.5V12.5H250V0Z" fill="#E8E8C6" />
+      <path d="M325 0H337.5V12.5H325V0Z" fill="#E8E8C6" />
+      <path d="M250 50H262.5V62.5H250V50Z" fill="#E8E8C6" />
+      <path d="M325 50H337.5V62.5H325V50Z" fill="#E8E8C6" />
+      <path d="M337.5 25H350V37.5H337.5V25Z" fill="#E8E8C6" />
+      <path d="M262.5 0H275V12.5H262.5V0Z" fill="#E8E8C6" />
+      <path d="M337.5 0H350V12.5H337.5V0Z" fill="#E8E8C6" />
+      <path d="M262.5 50H275V62.5H262.5V50Z" fill="#E8E8C6" />
+      <path d="M337.5 50H350V62.5H337.5V50Z" fill="#E8E8C6" />
+      <path d="M225 12.5H237.5V25H225V12.5Z" fill="#E8E8C6" />
+      <path d="M300 12.5H312.5V25H300V12.5Z" fill="#E8E8C6" />
+      <path d="M300 0H312.5V12.5H300V0Z" fill="#E8E8C6" />
+    </svg>
+  );
+}
--- elan-quest/next.config.ts
+++ elan-quest/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+module.exports = {
+  webpack(config: any) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
-
-export default nextConfig;
