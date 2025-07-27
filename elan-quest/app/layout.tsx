11771c9f3465badd3e26a8fdd4dfb297782c1c88
nathanalvares19 <nathanalvares2706@gmail.com>
2025-07-27 13:45:09 +0530

    temp

--- elan-quest/app/layout.tsx
+++ elan-quest/app/layout.tsx
@@ -31,10 +31,7 @@ export default function RootLayout({
     <html lang="en" className={`${pressStart2P.variable} ${raleway.variable}`}>
       <head>
         <title>Elan Quest</title>
-        <meta
-          name="description"
-          content="Created by Elan & Nvision Web Team 2026"
-        />
+        <meta name="description" content="Created by Nathan Alvares" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className="antialiased bg-[var(--background)] text-[var(--foreground)] min-h-screen flex flex-col">
@@ -42,11 +39,11 @@ export default function RootLayout({
         <div
           className={`flex-grow + ${
             menuOpen
-              ? "blur-[3px] transition duration-300 ease overflow-hidden select-none"
+              ? "blur-[3px] transition duration-300 ease select-none"
               : "transition duration-300 ease"
           }`}
         >
-          <main className="flex-grow">{children}</main>
+          <main className="">{children}</main>
         </div>
         <Footer menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </body>
