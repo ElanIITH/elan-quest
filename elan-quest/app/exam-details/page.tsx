5d30cb9a6b743c49e67447990a9d4c6a98fd780d
abhinav <srisaiabhinavreddyboreddy@gmail.com>
2025-12-05 22:39:15 +0530

    changed dates

--- elan-quest/app/exam-details/page.tsx
+++ elan-quest/app/exam-details/page.tsx
@@ -98,7 +98,7 @@ export default function ExamDetailsPage() {
               {[
                 { date: "August 7, 2025", label: "Registrations Open" },
                 { date: "December 9, 2025", label: "Registrations Close" },
-                { date: "December 10, 2025", label: "Quest Olympiad" },
+                { date: "December 21, 2025", label: "Quest Olympiad" },
                 { date: "Febraury, 2026", label: "Prize Distribution" },
               ].map((item, idx) => (
                 <div key={idx} className="mb-2">
